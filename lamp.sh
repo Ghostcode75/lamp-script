92d6c25b52f6de43a68bef4986c3bb1c1a3d1673
David Sanders <44623651+Ghostcode75@users.noreply.github.com>
2020-03-19 09:32:19 -0700

    swap file

--- lamp.sh
+++ lamp.sh
@@ -42,3 +42,10 @@ composer self-update
 usermod -a -G www-data $user
 echo "logout and back in to enable changes"
 
+sudo fallocate -l 1G /swapfile
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+echo "/swapfile swap swap defaults 0 0" > /etc/fstab
